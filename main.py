9d35bde97fae2d69c2fe44f15dc12ee6c761b0c5
ben1677216772 <ben1677216772@gmail.com>
2023-04-06 09:36:36 +0700

    first

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+import openai
+openai.api_key = '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'
+TOKEN = 'MTA5MzAwNTMwNTc2OTYzNTkwMQ.GmtYXa.kQ7PWS5-vt92HXnl1qTOGh--6W29v2CarvSet4'
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members =True
+
+bot = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} connected')
+    
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!คำถาม'):
+        question=message.content[7:]
+        await message.channel.send('กำลังประมวลผล...')
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": str(question)}
+            ])
+        answer = response.choices[0].message.content
+        await message.channel.send(str(answer))
+        
+        
+bot.run(TOKEN)
